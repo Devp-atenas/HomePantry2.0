b110b0e7be68a9eaa7d0c074d957728245dee770
Ronald Lopez <lopez.ronald@gmail.com>
2022-09-19 18:37:21 -0400

    jiooh
    jilouhhu

--- application/views/ListaHogares/ListaHogares.php
+++ application/views/ListaHogares/ListaHogares.php
@@ -1,32 +1,57 @@
-            <!-- Colocar Include Header y Menu 27jul21 -->
-            <?php $this->load->view('Plantillas/Header'); ?>
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1><i class="fa fa-plus-square aria-hidden=" true""></i>&nbsp;Listar Hogares</h1>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section> <!-- / section class="content header"> -->
-            <!-- Content Wrapper. Contains page content -->
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div id="TableHogaresPanelistas"></div>
-                                </div>
-                            </div>
-                        </div>
+<!-- Colocar Include Header y Menu 27jul21 -->
+<?php $this->load->view('Plantillas/Header'); ?>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1><i class="fa fa-plus-square aria-hidden=true"></i>&nbsp;Listar Hogares</h1>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section> <!-- / section class="content header"> -->
+<!-- Content Wrapper. Contains page content -->
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="TableHogaresPanelistas"></div>
                     </div>
                 </div>
-            </section>
-            <!-- /.content -->
-            <!-- Windows Modal Visualizar Registros-->
-            <?php $this->load->view('Plantillas/Footer'); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+<!-- Windows Modal CodigoBarraExistenteModal -->
+<div class="modal fade" id="hogarPerteneceMuestraModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fa-light fa-barcode-scan"></i>&nbsp;Hogar perteneciente a la Muestra</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                El hogar no puede desactivarse por pertenecer a la muestra, debe primero sustituir el hogar y luego desactivarlo
+            </div>
+            <div class="modal-footer">
+                <div class="row">
+                    <div class="col-12">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Entendido</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<?php $this->load->view('Plantillas/Footer'); ?>
 
 <script src="<?php echo base_url('jsHP/jsListaHogares.js') ?>"></script>
 <script>
--- jsHP/jsListaHogares.js
+++ jsHP/jsListaHogares.js
@@ -1,7 +1,49 @@
+function desactivarHogar(idPanelHogar){
+    var settings = {
+        "url": localStorage.getItem("urlApi")+'getIdMuestra4IdPanelHogar/' + idPanelHogar,
+        "method": "get",
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer " + localStorage.getItem('Token')
+        }
+    }
+    $.ajax(settings).done(function(response) {
+        
+        if (response.data[0].id_muestra != 0){
+            $('#hogarPerteneceMuestraModal').modal('show');
+        }else{
+
+        }
+
+
+    }).fail(function(jqXHR, textStatus) {
+        if (jqXHR.status == 400) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 10000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            Toast.fire({
+                title: 'Su Session ha Expirado',
+                confirmButtonText: `Ok`,
+            })
+            window.location = '/homepantry20/index.php';
+        }
+    })
+}
+
+
 function cargarTablaHogares(idDivTabla){ 
     var bottomAcciones = function(cell, formatterParams){
         var value = cell.getRow().getData().Id_PanelHogar;
-        return  "<a id='jjj' href='#' onclick='EditAction("+value+"); return false;' ><i class='bi bi-house-fill' data-toggle='tooltip' data-placement='top' title='Cargar Hogar'></i></a>";
+        return  "<a id='jjj' href='#' onclick='EditAction("+value+"); return false;' ><i class='bi bi-house-fill' data-toggle='tooltip' data-placement='top' title='Cargar Hogar'></i></a>"
+                +"<a id='jjj' href='#' onclick='desactivarHogar("+value+"); return false;' ><i class='bi bi-file-x text-danger' data-toggle='tooltip' data-placement='top' title='Desactivar Hogar'></i></a>";
     };
     
     table = new Tabulator(idDivTabla, {
@@ -23,6 +65,7 @@ function cargarTablaHogares(idDivTabla){
         columns:[
             {title:"Id_PanelHogar:", field:"Id_PanelHogar", sorter:"number", headerFilter:true},
             {title:"CodigoHogar", field:"CodigoHogar", sorter:"string", headerFilter:true},
+            {title:"Muestra", field:"Id_Muestra", sorter:"string", headerFilter:true},
             {title:"Responsable", field:"NombreR", sorter:"string", headerFilter:true},
             {title:"Cedula", field:"cedulaR", sorter:"number", headerFilter:true},
             {title:"Jefe", field:"NombreJJ", sorter:"string", headerFilter:true},
