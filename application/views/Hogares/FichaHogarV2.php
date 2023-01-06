9dbd8c7a6f4ca26c4e4df4c154447c06f787b864
Ronald Lopez <lopez.ronald@gmail.com>
2023-01-06 16:57:03 -0400

    sz<d
    sdfsd

--- application/views/Hogares/FichaHogarV2.php
+++ application/views/Hogares/FichaHogarV2.php
@@ -995,7 +995,7 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <div class="inputText font-weight-bold">Especifique:</div>
-                                <input type="text" class="form-control input-sm" id="especifiqueTipoVivienda" placeholder="Especifique ..." disabled="disabled">
+                                <input type="text" class="form-control input-sm" id="especifiqueTipoVivienda"  name="especifiqueTipoVivienda" placeholder="Especifique ..." disabled="disabled">
                             </div>
                         </div>
                     </div>
@@ -1003,7 +1003,7 @@
                         <div class="col-md-3">
                             <div class="form-group">
                                 <div class="inputText font-weight-bold">Metros Cuadrados:</div>
-                                <select class="custom-select form-control-border" id="metrosVivienda">
+                                <select class="custom-select form-control-border" id="metrosVivienda" name="metrosVivienda">
                                 </select>
                             </div>
                         </div>
@@ -1022,7 +1022,7 @@
                         <div class="col-md-3">
                             <div class="form-group">
                                 <div class="inputText font-weight-bold">Numero total de puntos de luz:</div>
-                                <select class="custom-select form-control-border" id="puntosLuz">
+                                <select class="custom-select form-control-border" id="puntosLuz" name="puntosLuz">
                                 </select>
                             </div>
                         </div>
@@ -1031,7 +1031,7 @@
                         <div class="col-md-3">
                             <div class="form-group">
                                 <div class="inputText font-weight-bold">Ocupación actual de la vivienda:</div>
-                                <select class="custom-select form-control-border" id="ocupacionVivienda">
+                                <select class="custom-select form-control-border" id="ocupacionVivienda" name="ocupacionVivienda">
                                 </select>
                             </div>
                         </div>
@@ -3061,113 +3061,6 @@
 
 
     // Características y tenencia de la vivienda PASO 5
-    $("#guardar-paso-5").click(function() {
-        var camposVacios = "";
-
-        if ($("#tipoVivienda").val() == "12") {
-            if ($("#especifiqueTipoVivienda").val() == "") {
-                camposVacios += "Especifique tipo de vivienda<br>";
-            }
-        }
-        if ($("#numeroAmbientes").val() == "") {
-            camposVacios += "Numero total de ambientes<br>";
-        }
-        if ($("#numeroBanos").val() == "") {
-            camposVacios += "Numero total de baños<br>";
-        }
-        if ($("#ocupacionVivienda").val() == "6") {
-            if ($("#especifiqueOcupacionVivienda").val() == "") {
-                camposVacios += "Especifique  ocupación actual de la vivienda<br>";
-            }
-        }
-        if (camposVacios != "") {
-            $(function() {
-                $('#modal-body').html(camposVacios);
-            });
-            $('#myModal').modal('show');
-        } else {
-            if (localStorage.getItem("flagActividad") !== null){
-                flagNuevoHogar = 0;
-            }else{
-                flagNuevoHogar = 1;
-            }
-
-            var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": '<?php echo urlApi; ?>updateCaracteristicaHogar/',
-                "method": "POST",
-                "headers": {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Bearer " + localStorage.getItem('Token')
-                },
-                "data": {
-                    "Id_Hogar": $("#identificacion2Hogar").val(),
-                    "Id_TipoVivienda": $("#tipoVivienda").val(),
-                    "OtroTipoVivienda": $("#especifiqueTipoVivienda").val(),
-                    "id_Metros": $("#metrosVivienda").val(),
-                    "NumeroAmbientes": $("#numeroAmbientes").val(),
-                    "NumeroBanos": $("#numeroBanos").val(),
-                    "id_PuntosLuz": $("#puntosLuz").val(),
-                    "Id_OcupacionVivienda": $("#ocupacionVivienda").val(),
-                    "OtroOcupacionVivienda": $("#especifiqueOcupacionVivienda").val(),
-                    "Id_MontoVivienda": $("#montoVivienda").val(),
-                    "flagNuevoHogar":flagNuevoHogar
-                }
-            }
-            $.ajax(settings).done(function(response) {
-                var idHogar = $("#identificacion2Hogar").val();
-                if (response.isFichaCompleta==1){
-                    Bitacora(localStorage.getItem("IdUsuario"),localStorage.getItem("IP"),"Se actualizo Características y tenencia de la vivienda (Paso 5) Falta Puntaje NSE(idPanelHogar)",$("#identificacion2Hogar").val(),"U");
-                    AlertaHogarSinRecalcularNSE($("#identificacion2Hogar").val());
-                }else{
-                    Bitacora(localStorage.getItem("IdUsuario"),localStorage.getItem("IP"),"Se Guardo Características y tenencia de la vivienda (Paso 5) (idPanelHogar)",$("#identificacion2Hogar").val(),"C");
-                }
-
-                if (localStorage.getItem('flagActividad') !== null){
-                    guardarCamposPorActividad(idHogar,5);
-                }
-                const Toast = Swal.mixin({
-                    toast: true,
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 10000,
-                    timerProgressBar: true,
-                    didOpen: (toast) => {
-                        toast.addEventListener('mouseenter', Swal.stopTimer)
-                        toast.addEventListener('mouseleave', Swal.resumeTimer)
-                    }
-                })
-                Toast.fire({
-                    icon: 'success',
-                    title: response.message,
-                    confirmButtonText: `Ok`,
-                })  
-            }).fail(function(jqXHR, textStatus) {
-                if (jqXHR.status == 400) {
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 10000,
-                        timerProgressBar: true,
-                        didOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    })
-                    Toast.fire({
-                        icon: 'info',
-                        title: 'Su Session ha Expirado',
-                        confirmButtonText: `Ok`,
-                    })
-                    var form = document.querySelector('#FormPaisEdit');
-                    form.reset();
-                    window.location = '/homepantry20/index.php';
-                }
-            })
-        }
-    });
     // Servicios Públicos PASO 6
     $("#guardar-paso-6").click(function() {
         var camposVacios = "";
--- jsHP/jsValidarFichaHogar.js
+++ jsHP/jsValidarFichaHogar.js
@@ -87,10 +87,6 @@ $(document).ready(function() {
 
 // Ubicacion del hogar (PASO 1)
 $("#guardar-paso-1").click(function() {
-
-
-
-    
     if ($("#formUbicacionHogar").valid()) {
         var Ind_Vivienda;
         var Ind_Edificio;
@@ -103,7 +99,7 @@ $("#guardar-paso-1").click(function() {
             Ind_Edificio = 1;
 
         }
-        alert(111);
+        //alert(111);
         var idHogar2 = $("#identificacion2Hogar").val();
         const fecha = new Date();
         const YYYY = fecha.getFullYear();
@@ -196,4 +192,222 @@ $("#guardar-paso-1").click(function() {
             }
         }) 
     }    
+});
+
+$(document).ready(function() {
+    $('#formCaracteristicaVivienda').validate({
+        rules: {
+            tipoVivienda: {
+                required: true
+            },
+            especifiqueTipoVivienda: {
+                required: true
+            },
+            metrosVivienda: {
+                required: true
+            },
+            numeroAmbientes: {
+                required: true
+            },
+            numeroBanos: {
+                required: true
+            },
+            puntosLuz: {
+                required: true
+            },
+            ocupacionVivienda: {
+                required: true
+            },
+            especifiqueOcupacionVivienda: {
+                required: true
+            },
+            montoVivienda: {
+                required: true
+            },
+            selectTecho: {
+                required: true
+            },
+            selectPiso : {
+                required: true
+            },
+            selectParedes : {
+                required: true
+            },
+            Pintura : {
+                required: true
+            },
+            selectPEstacionamiento : {
+                required: true
+            },
+            selectPlanVacacional : {
+                required: true
+            },
+            selectViajeVacacional : {
+                required: true
+            },
+            
+            
+        },
+        messages: {
+            estadoHogar: {
+                required: "Ingrese estado"
+            },
+            ciudadHogar: {
+                required: "Ingrese ciudad"
+            },
+            municipioHogar: {
+                required: "Ingrese municipio"
+            },
+            parroquiaHogar: {
+                required: "Ingrese parroquia"
+            },
+            calle: {
+                required: "Ingrese calle"
+            },
+            selectZona: {
+                required: "Ingrese zona"
+            },
+            vivienda: {
+                required: "Ingrese tipo vivienda"
+            },
+            telefonoHogar: {
+                required: "Ingrese telefono local"
+            },
+            referenciaHogar: {
+                required: "Ingrese referencia"
+            },
+            coordenadaX: {
+                required: "Ingrese coordenada X"
+            },
+            coordenadaY : {
+                required: "Ingrese coordenada Y"
+            },
+            
+        },
+        errorElement: 'span',
+        errorPlacement: function(error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.form-group').append(error);
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        }
+    });
+});
+
+
+
+
+
+
+
+$("#guardar-paso-5").click(function() {
+    var camposVacios = "";
+
+    if ($("#tipoVivienda").val() == "12") {
+        if ($("#especifiqueTipoVivienda").val() == "") {
+            camposVacios += "Especifique tipo de vivienda<br>";
+        }
+    }
+    if ($("#numeroAmbientes").val() == "") {
+        camposVacios += "Numero total de ambientes<br>";
+    }
+    if ($("#numeroBanos").val() == "") {
+        camposVacios += "Numero total de baños<br>";
+    }
+    if ($("#ocupacionVivienda").val() == "6") {
+        if ($("#especifiqueOcupacionVivienda").val() == "") {
+            camposVacios += "Especifique  ocupación actual de la vivienda<br>";
+        }
+    }
+    if (camposVacios != "") {
+        $(function() {
+            $('#modal-body').html(camposVacios);
+        });
+        $('#myModal').modal('show');
+    } else {
+        if (localStorage.getItem("flagActividad") !== null){
+            flagNuevoHogar = 0;
+        }else{
+            flagNuevoHogar = 1;
+        }
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": localStorage.getItem("urlApi")+'updateCaracteristicaHogar/',
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Bearer " + localStorage.getItem('Token')
+            },
+            "data": {
+                "Id_Hogar": $("#identificacion2Hogar").val(),
+                "Id_TipoVivienda": $("#tipoVivienda").val(),
+                "OtroTipoVivienda": $("#especifiqueTipoVivienda").val(),
+                "id_Metros": $("#metrosVivienda").val(),
+                "NumeroAmbientes": $("#numeroAmbientes").val(),
+                "NumeroBanos": $("#numeroBanos").val(),
+                "id_PuntosLuz": $("#puntosLuz").val(),
+                "Id_OcupacionVivienda": $("#ocupacionVivienda").val(),
+                "OtroOcupacionVivienda": $("#especifiqueOcupacionVivienda").val(),
+                "Id_MontoVivienda": $("#montoVivienda").val(),
+                "flagNuevoHogar":flagNuevoHogar
+            }
+        }
+        $.ajax(settings).done(function(response) {
+            var idHogar = $("#identificacion2Hogar").val();
+            if (response.isFichaCompleta==1){
+                Bitacora(localStorage.getItem("IdUsuario"),localStorage.getItem("IP"),"Se actualizo Características y tenencia de la vivienda (Paso 5) Falta Puntaje NSE(idPanelHogar)",$("#identificacion2Hogar").val(),"U");
+                AlertaHogarSinRecalcularNSE($("#identificacion2Hogar").val());
+            }else{
+                Bitacora(localStorage.getItem("IdUsuario"),localStorage.getItem("IP"),"Se Guardo Características y tenencia de la vivienda (Paso 5) (idPanelHogar)",$("#identificacion2Hogar").val(),"C");
+            }
+
+            if (localStorage.getItem('flagActividad') !== null){
+                guardarCamposPorActividad(idHogar,5);
+            }
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 10000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            Toast.fire({
+                icon: 'success',
+                title: response.message,
+                confirmButtonText: `Ok`,
+            })  
+        }).fail(function(jqXHR, textStatus) {
+            if (jqXHR.status == 400) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 10000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                })
+                Toast.fire({
+                    icon: 'info',
+                    title: 'Su Session ha Expirado',
+                    confirmButtonText: `Ok`,
+                })
+                var form = document.querySelector('#FormPaisEdit');
+                form.reset();
+                window.location = '/homepantry20/index.php';
+            }
+        })
+    }
 });
